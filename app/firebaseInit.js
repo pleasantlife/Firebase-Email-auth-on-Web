5b301a98c6caeea740b28913c47af0d0960a2d05
pleasantlife <pleasantlife@gmail.com>
2017-11-14 14:01:06 +0900

    Add files via upload

---
+++ app/firebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase(Firebase Sample Project)
+var config = {
+    apiKey: "AIzaSyBB2h_gfvvRHgKfW446aRCA9rwQcU6x4uk",
+    authDomain: "fir-sample2-9237c.firebaseapp.com",
+    databaseURL: "https://fir-sample2-9237c.firebaseio.com",
+    projectId: "fir-sample2-9237c",
+    storageBucket: "fir-sample2-9237c.appspot.com",
+    messagingSenderId: "276221771550"
+  };
+  firebase.initializeApp(config);
